49c64ca40b53add1a91cdd2256371a1dc919f93f
Luka Jankovic <74156547+ragacijane@users.noreply.github.com>
2024-04-18 14:28:54 -0700

    Add files via upload

---
+++ src/test/java/com/RealEstateRental/RealEstateRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RealEstateRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
