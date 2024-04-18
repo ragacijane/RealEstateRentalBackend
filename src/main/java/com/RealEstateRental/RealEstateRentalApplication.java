32f41173e96577c3cc8ba1461a2050c25a4b8414
Luka Jankovic <74156547+ragacijane@users.noreply.github.com>
2024-04-18 14:36:35 -0700

    Add files via upload

---
+++ src/main/java/com/RealEstateRental/RealEstateRentalApplication.java
@@ -0,0 +1,13 @@
+package com.RealEstateRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateRentalApplication.class, args);
+	}
+
+}
